fd4686b8deeadd65ce53d293afef9d079b524f8c
Donnie Heard <dhearddev@gmail.com>
2018-08-16 20:21:36 -0700

    added JAWSDB conditional

--- config/connection.js
+++ config/connection.js
@@ -2,19 +2,25 @@ require("dotenv").config();
 
 // Set up MySQL connection.
 const mysql = require("mysql");
+const connection;
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    /*
-    TA: 
-        Option1: Create an .env file and create a SQL_PASSWORD var w/ Password
-        Option2: Hard-code Password
-    */
-    password: process.env.SQL_PASSWORD,
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        /*
+        TA: 
+            Option1: Create an .env file and create a SQL_PASSWORD var w/ Password
+            Option2: Hard-code Password
+        */
+        password: process.env.SQL_PASSWORD,
+        database: "burgers_db"
+    });
+}
 
 connection.connect(function(err) {
     if(err){
